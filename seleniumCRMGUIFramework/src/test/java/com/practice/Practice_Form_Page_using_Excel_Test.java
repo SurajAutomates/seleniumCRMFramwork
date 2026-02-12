dadee50df42aaa7b680e015b859148c52c496c76
surajkadam <surajkadam@localhost>
2026-02-12 13:19:36 +0530

    commit50

--- seleniumCRMGUIFramework/src/test/java/com/practice/Practice_Form_Page_using_Excel_Test.java
+++ seleniumCRMGUIFramework/src/test/java/com/practice/Practice_Form_Page_using_Excel_Test.java
@@ -6,5 +6,6 @@ public class Practice_Form_Page_using_Excel_Test {
 	public static void main(String[] args) {
 		ExcelUtility excelutility = new ExcelUtility();
 		System.out.println("hi");
+		System.out.println("bye");
 	}
 }
